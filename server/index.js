f7f24067f81d6cc18b9c71c6f495dc94d6582a0e
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-10-29 23:28:20 +0530

    server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,12 @@ app.use(express.json());
 
 const PORT=process.env.PORT || 3002;
 
+app.get("/",(req,res)=>{
+ res.send("Server is live  ");
+ console.log("Server is running !")
+
+})
+
 app.use(require('./app.js'));
 
 app.listen(PORT,_=>console.log("Server is running at port ",PORT));
